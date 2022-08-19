59237eb35d71a3cb4b574d3e6b9a8c369c1f9c34
Anassiddiqui-cedcoss <mohdanassiddiqui@cedcoss.com>
2022-08-19 16:34:32 +0530

    hello world

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ClassComponent from './components';
+
+function App() {
+  return (
+    <div>
+      <header className="App-header">
+      <ClassComponent/>
+      </header>
+       
+    </div>
+  );
+}
+
+export default App;
